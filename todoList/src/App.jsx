b090d5207192a07b1144705b1040f6dc0c437f13
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-09-18 16:47:20 +0500

    .

--- todoList/src/App.jsx
+++ todoList/src/App.jsx
@@ -1,13 +1,15 @@
 
+import { useState } from "react"
 import CreatePost from "./CreatePost"
 import Post from "./View/post"
 
 
 function App() {
+  const [posts, setPosts] = useState([])
   return (
     <div className="bg-orange-300 h-screen">
-    <Post/>
-    <CreatePost/>
+    <Post posts={posts} setPosts={setPosts}/>
+    <CreatePost posts={posts} setPosts={setPosts}/>
     </div>
   )
 }
--- todoList/src/View/Post.jsx
+++ todoList/src/View/Post.jsx
@@ -2,9 +2,9 @@ import {  useEffect } from "react"
 import { useState } from "react"
 import { getPosts } from "../Services/postApiHitting"
 // import Error from "./Error"
+/* eslint-disable react/prop-types */
+function Post({posts,setPosts}){
 
-function Post(){
-const [posts, setPosts] = useState([])
 // const  [error, setError] = useState(null)
 const [showDescription , setShowDescription] = useState([])
 
@@ -15,42 +15,35 @@ useEffect(function(){
             setPosts(data)
             console.log(data)
         } catch (error) {
-    //    setError(error.message)
         console.error(error)
         }
     }
     fetchData()
 },[])
 
-function handleChange(i){
-    setShowDescription((prevState)=>({
-        ...prevState,
-        [i]: !prevState[i]
-    }))
-}
+    function handleChange(i){
+        setShowDescription((prevState)=>({
+            ...prevState,
+            [i]: !prevState[i]
+        }))
+    }
 
     return <div>
         <h1 className="font-bold text-2xl text-center m-2">
             Post title
         </h1>
         {/* {error ? <Error>{error} </Error>:  */}
-        <ul className="from-neutral-600 bg-orange-300 grid grid-cols-2">
-           {posts.map((post, index) => (
+        <ul className="from-neutral-600 bg-orange-300 grid grid-cols-2 items-start ">
+           {posts?.slice(0,10).map((post, index) => (
             <>
             <div className="flex">
-
-            <li key={post.index} className="border my-2 p-2 mx-2 bg-orange-100 rounded-sm">{post.title} {showDescription[index] &&  <p className="m-2"> {post.body}</p>}
-
+            <li key={post.index} className="border my-2 p-2 mx-2 bg-orange-300 rounded-md">{post.title} {showDescription[index] &&  
+                <p className="p-2 bg-orange-200"> {post.body}</p>}
             <button onClick={()=>handleChange(index)} className="ml-2 border-2 p-1 border-orange-950 rounded-md ">{showDescription[index] ? "Hide" : "Show"} </button>
             </li>
-            
             </div>
-
-        
             </> 
           ))}
-         
-        
         </ul>
         {/* } */}
     </div>
