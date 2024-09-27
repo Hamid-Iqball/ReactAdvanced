6e6fe27dfb58de7c5c3b365de9b71a66ebc7a173
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2024-09-27 11:55:27 +0500

    initial commit in CRUD

---
+++ CRUD/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+
+  return (
+    <div className='bg-slate-500 h-screen w-screen p-6'>
+<h1 className='text-slate-50 text-3xl font-semibold  my-4 text-center'>CRUD Operation with Zustand</h1>
+    </div>
+  )
+}
+
+export default App
